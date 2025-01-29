034c4855222f71792ecfb2d18ada365a22a9c6d0
dahlia-dj <dahliadjoumessi18@gmail.com>
2025-01-29 13:39:13 +0100

    init

--- src/clean.py
+++ src/clean.py
@@ -1,13 +1,13 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
-from store import extract_from_hdfs
+from store import extract_from_hdfs,store_to_hdfs
 from paths_server import *
 
 # Nettoyage des données
 def data_cleaned(df:DataFrame):
-   df = extract_from_hdfs(datalake_path)
-   df_clean = df.nadrop().dropDuplicates() #suppression des valeurs nulles et les doublons
-
+    df = extract_from_hdfs(datalake_path)
+    df_clean = df.dropna(how:"all").dropDuplicates() #suppression des valeurs nulles et les doublons
+    store_to_hdfs(df_clean,datastore_path)
 
 
 
