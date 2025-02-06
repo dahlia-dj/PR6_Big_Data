321eca6f33fade0e71474cd8a92c2a8b0004adaa
dahlia-dj <dahliadjoumessi18@gmail.com>
2025-02-06 07:16:30 +0000

    Mise à jour

--- src/clean.py
+++ src/clean.py
@@ -1,25 +1,53 @@
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.utils import *
+from pyspark.sql.functions import count, col, explode, from_json, year, month, round, to_timestamp
+from pyspark.sql.types import StructType, StructField, StringType, ArrayType
 
-# Nettoyage des données
-def df_cleaned(df:DataFrame):
-    #df = extract_from_hdfs(datalake_path) #recuperation des données depuis hdfs
-    df1= df.dropna(how="all")
-    df_clean= df1.dropDuplicates() #suppression des valeurs nulles et les doublons
-    return df_clean
+# Initialiser Spark
+spark = SparkSession.builder.appName("TransportAnalysis").getOrCreate()
 
-if __name__=="__main__": # type: ignore
+# Charger les fichiers CSV
+arrets_lignes_df = spark.read.option("header", True).option("sep", ";").csv("/home/vboxuser/PR6_Big_Data/data/arrets-lignes.csv")
+disruptions_df = spark.read.option("header", True).csv("/home/vboxuser/PR6_Big_Data/data/disruptions.csv")
+lines_df = spark.read.option("header", True).csv("/home/vboxuser/PR6_Big_Data/data/lines.csv")
 
-    from store import store_to_hdfs, extract_from_hdfs
+# Nettoyage des arrêts (conversion des coordonnées)
+arrets_lignes_df = arrets_lignes_df.withColumn("stop_lon", col("stop_lon").cast("double"))
+arrets_lignes_df = arrets_lignes_df.withColumn("stop_lat", col("stop_lat").cast("double"))
+arrets_lignes_df = arrets_lignes_df.dropna(subset=["stop_name", "stop_lon", "stop_lat"])
 
-    spark = SparkSession.builder.appName('csv_extraction').getOrCreate()
-    data_static = extract_from_hdfs(spark,"hdfs://localhost:9000/user/ubuntu/datalake/data_csv/part-00000-aa692112-eef1-40bf-b296-055998ad0458-c000.csv") # type: ignore
-    data_cleaned = df_cleaned(data_static)
-    data_cleaned.show() 
-    #store_to_hdfs(data_cleaned,"hdfs://localhost:9000/user/ubuntu/datastore/data_cleaned.parquet")
+# Extraction des lignes impactées dans disruptions
+impacted_schema = ArrayType(StructType([
+    StructField("type", StringType(), True),
+    StructField("id", StringType(), True),
+    StructField("name", StringType(), True)
+]))
+disruptions_df = disruptions_df.withColumn("impactedObjects", from_json(col("impactedObjects"), impacted_schema))
+disruptions_exploded = disruptions_df.withColumn("line_id", explode(col("impactedObjects.id")))
+disruptions_exploded = disruptions_exploded.drop("impactedObjects")
 
+# Conversion des dates
+disruptions_exploded = disruptions_exploded.withColumn("lastUpdate", to_timestamp(col("lastUpdate"), "yyyyMMdd'T'HHmmss"))
 
+# Nombre de perturbations par ligne
+perturbations_par_ligne = disruptions_exploded.groupBy("line_id").agg(count("id").alias("nombre_perturbations")).orderBy(col("nombre_pertubations").desc())
+
+# Evolution des perturbations dans le temps
+disruptions_time = disruptions_exploded.withColumn("year", year(col("lastUpdate"))) \
+                                       .withColumn("month", month(col("lastUpdate")))
+disruptions_by_time = disruptions_time.groupBy("year", "month").count()
+
+# Nombre d'arrêts par ligne
+arrets_par_ligne = arrets_lignes_df.groupBy("id").agg(count("stop_id").alias("nombre_arrets")).orderBy(col("nombre_arrets").desc())
+
+# Regrouper les insights par ligne
+final_df = lines_df.join(arrets_par_ligne, lines_df.id == arrets_par_ligne.id, "left") \
+                   .join(perturbations_par_ligne, lines_df.id == perturbations_par_ligne.line_id, "left") \
+                   .select(lines_df.id, lines_df.name, "nombre_arrets", "nombre_perturbations")
+
+# Sauvegarde des résultats en format Parquet sur HDFS
+final_df.write.mode("overwrite").csv("/home/vboxuser/PR6_Big_Data/data/final_data.csv")
+
+# Afficher les résultats
+final_df.show()
 
 
--- src/extract.py
+++ src/extract.py
@@ -29,9 +29,19 @@ def convert_to_dataframe(spark,data):
 
 if __name__=="__main__": # type: ignore
 
-    from store import store_to_hdfs
-
+#creation de la session spark
     spark = SparkSession.builder.appName('csv_extraction').getOrCreate()
-    data_static = extract_csv(spark,"/home/ubuntu/Documents/PR6-BIGDATA/data/dioxide.csv") # type: ignore
-    data_static.show() 
-    store_to_hdfs(data_static,"hdfs://localhost:9000/user/ubuntu/datalake/data.parquet")   
+
+#ingestion et stockage des données statiques
+    data_static = extract_csv(spark,"/home/vboxuser/PR6_Big_Data/data/arrets-lignes.csv") # type: ignore
+    #data_static.show(5) 
+    store_to_hdfs(data_static,"hdfs://localhost:9000/user/ubuntu/data_static/arrets.parquet")   
+
+#ingestion et stockage des données dynamiques
+    data_dynamic = extract_csv(spark,"/home/vboxuser/PR6_Big_Data/data/disruptions.csv") # type: ignore
+    #data_dynamic.show() 
+    store_to_hdfs(data_dynamic,"hdfs://localhost:9000/user/ubuntu/data_dynamic/disruptions.parquet")
+
+    data_dynamic2 = extract_csv(spark,"/home/vboxuser/PR6_Big_Data/data/lines.csv") # type: ignore
+    data_dynamic2.show(5) 
+    store_to_hdfs(data_dynamic2,"hdfs://localhost:9000/user/ubuntu/data_dynamic/lines.parquet")
